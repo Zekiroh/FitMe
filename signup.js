99d00b76c7a21031683d23086e123d3cedb3f737
Zekiroh <139104446+Zekiroh@users.noreply.github.com>
2025-03-01 03:20:03 +0800

    Update signup.js

--- signup.js
+++ signup.js
@@ -3,10 +3,10 @@ function togglePassword(fieldId) {
     const toggleIcon = passwordInput.nextElementSibling.querySelector('.toggle-icon');
     if (passwordInput.type === 'password') {
         passwordInput.type = 'text';
-        toggleIcon.src = 'assets/icons/hide-password.png';
+        toggleIcon.src = 'hide-password.png';
     } else {
         passwordInput.type = 'password';
-        toggleIcon.src = 'assets/icons/show-password.png';
+        toggleIcon.src = 'show-password.png';
     }
 }
 
